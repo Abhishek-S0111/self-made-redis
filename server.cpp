03c6620c86e52b823fa69fa792ff76bb5677fac4
Abhishek Sinha <abhisheksinha42069@gmail.com>
2025-03-16 16:02:37 +0000

    Introduce a new System Call

--- server.cpp
+++ server.cpp
@@ -9,6 +9,14 @@ int main(int argv, char * argc[]){
         SOCK_STREAM :  for TCP   
     */
 
-    
+    //now introduce a new syscall
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+    /*
+        setsockopt() : used to configure various aspects of a socket
+        Above, we are enabling the SO_REUSEADDR option which allows us to reuse the local addresses and ports.
+        It is required as more than one socket needs to bind to the UDP port.
+         
+    */
 
 }
