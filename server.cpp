029f19fe1c14934c620846edaaaa0048b742d080
Abhishek Sinha <abhisheksinha42069@gmail.com>
2025-03-16 16:01:54 +0000

    Get a Socket ID  #1

--- server.cpp
+++ server.cpp
@@ -2,6 +2,13 @@
 
 
 int main(int argv, char * argc[]){
+    //first, obtain a socket ID
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    /*
+        AF_INET     :  for IPv4
+        SOCK_STREAM :  for TCP   
+    */
+
     
 
 }
